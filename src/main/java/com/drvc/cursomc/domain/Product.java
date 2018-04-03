50dacc150cec691e9fc3bff785da688c89c1ec10
Diego Cassiano <diegorvcassiano@gmail.com>
2018-04-03 20:54:47 -0300

    Included the Product domain and made the associations many-to-many with category

--- src/main/java/com/drvc/cursomc/domain/Product.java
+++ src/main/java/com/drvc/cursomc/domain/Product.java
@@ -8,27 +8,35 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Category implements Serializable{	
+public class Product implements Serializable{	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
+	@GeneratedValue(strategy=GenerationType.IDENTITY)	
+	private Integer id;	
 	private String name;
+	private Double price;
 	
-	@ManyToMany(mappedBy="categories")
-	private List<Product> products = new ArrayList<>();
-	
-	public Category() {		
-	}
+	@ManyToMany
+	@JoinTable(
+			name="PRODUCT_CATEGORY",
+			joinColumns=@JoinColumn(name="product_id"),
+			inverseJoinColumns=@JoinColumn(name="category_id")
+	)
+	private List<Category> categories = new ArrayList<>();
 
-	public Category(Integer id, String name) {
+	public Product() {}
+	
+	public Product(Integer id, String name, Double price) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
 	}
 
 	public Integer getId() {
@@ -45,14 +53,22 @@ public class Category implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
-	}	
-
-	public List<Product> getProducts() {
-		return products;
 	}
 
-	public void setProducts(List<Product> products) {
-		this.products = products;
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
 	}
 
 	@Override
@@ -71,14 +87,13 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}		
-	
+	}	
 	
 }
