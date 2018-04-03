724ca184bd44eb0348b69c0fc2f2d2f52edfbcc1
Diego Cassiano <diegorvcassiano@gmail.com>
2018-04-03 20:15:55 -0300

    Created instatiation command for category

--- src/main/java/com/drvc/cursomc/CursomcApplication.java
+++ src/main/java/com/drvc/cursomc/CursomcApplication.java
@@ -1,12 +1,29 @@
 package com.drvc.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.drvc.cursomc.domain.Category;
+import com.drvc.cursomc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
