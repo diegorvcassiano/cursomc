7bed35a9d6259fdb94c3b256ffd9ebb455cf0626
Diego Cassiano <diegorvcassiano@gmail.com>
2018-04-03 22:20:05 -0300

    Created State and City dao mappings.

--- src/main/java/com/drvc/cursomc/CursomcApplication.java
+++ src/main/java/com/drvc/cursomc/CursomcApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.drvc.cursomc.domain.Category;
+import com.drvc.cursomc.domain.City;
 import com.drvc.cursomc.domain.Product;
+import com.drvc.cursomc.domain.State;
 import com.drvc.cursomc.repositories.CategoryRepository;
+import com.drvc.cursomc.repositories.CityRepository;
 import com.drvc.cursomc.repositories.ProductRepository;
+import com.drvc.cursomc.repositories.StateRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private CityRepository cityRepository;
+	@Autowired
+	private StateRepository stateRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -43,5 +51,18 @@ public class CursomcApplication implements CommandLineRunner{
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
+		
+		State st1 = new State(null, "Minas Gerais");
+		State st2 = new State(null, "São Paulo");
+		
+		City c1 = new City(null, "Uberlândia", st1);
+		City c2 = new City(null, "São Paulo", st2);
+		City c3 = new City(null, "Campinas", st2);
+		
+		st1.getCities().addAll(Arrays.asList(c1));
+		st2.getCities().addAll(Arrays.asList(c2,c3));
+		
+		stateRepository.saveAll(Arrays.asList(st1,st2));
+		cityRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 }
